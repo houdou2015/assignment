cdbdf471df9b62aa4998a056eae0d1b0f349a9c1
houdou2015 <791255763@qq.com>
2015-07-17 15:25:41 +0800

    submit

--- ID.java
+++ ID.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class ID {
-	public static boolean id_text(String id){
+	public static boolean id_text(String id) {
 		int sum=0;
 		int[] a={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
 		int array[]=new int[17];
-		for(int i=0;i<17;i++){
-			array[i]=Integer.parseInt(String.valueOf(id.charAt(i)));     //把从键盘输入的数强制转换成字符串，在转换成整型     
-			sum+=a[i]*array[i];
+		try{
+			for(int i=0;i<17;i++){
+				array[i]=Integer.parseInt(String.valueOf(id.charAt(i)));     //把从键盘输入的数强制转换成字符串，在转换成整型     
+				sum+=a[i]*array[i];
+			}
+			
+		}catch(Exception h){
+			System.out.println("异常");
 		}
 		int figure=sum%11;
 		try{
-			int n=Integer.parseInt(String.valueOf(id.charAt(17)));    //把从键盘输入的数强制转换成字符串，在转换成整型     
+			int n=Integer.parseInt(String.valueOf(id.charAt(17)));          //把从键盘输入的数强制转换成字符串，在转换成整型     
 			if(figure<=1){
 				return (figure==0 && n==1)||(figure==1 && n==0);
 			}else if((figure==(12-n))||(figure<=10)){
