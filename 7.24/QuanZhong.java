956204dc8275a6c5626ed3aab3740ca5a020c103
houdou2015 <791255763@qq.com>
2015-07-24 12:24:40 +0800

    updata

---
+++ 7.24/QuanZhong.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class Vocabulary {
+	private static int i;
+	//计算出文章中每个单词出现的次数。
+public static int isConut(int cnt[i]){
+	ArrayList<Integer> ch1=new ArrayList<Integer>();
+	int[] cnt = new int[ch1.lenght()];
+	for(int i=0;i<ch1.length ();i++)
+	{
+		for(int j=i+1;j<ch1.length()-j;j++)
+		{
+		  if(ch1[i]==ch1[j])
+		  {
+			  cnt[i]++;
+		  }
+		 }
+		return cnt[i];
+	}
+}
+//计算权重
+public static double QuanZhong()
+{
+	String[] ch2={"am", "is", "are", "was"," been", "has", "have", "had"," a", "an", "the", "in", "at", "on", "to", "or"};
+	double totalQuanZhong=0;
+	
+	
+}
+public static void main(String[] args) {
+
+
+	}
+
+}
